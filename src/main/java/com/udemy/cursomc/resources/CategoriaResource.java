87cf4d775375c0ca994aa40492ae2e9a041c9b8d
Andre <andcarreiro@gmail.com>
2018-08-07 12:14:36 -0300

    Criando repository e service paa categoria

---
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.udemy.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.services.CategoriaSevice;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaSevice service;
+	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find (@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
